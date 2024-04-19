731c1dc9cb1679cc679e2cfeb0faaf126d252506
qzheng75 <zhengqianyu0227@gmail.com>
2024-04-19 15:11:50 -0400

    Finished VGG fine-tuning

--- code/audio_toolbox/models.py
+++ code/audio_toolbox/models.py
@@ -13,7 +13,7 @@ class LinearModel(nn.Module):
                 layers.append(nn.Linear(input_size, hidden_dim))
             else:
                 layers.append(nn.Linear(hidden_dim, hidden_dim))
-            layers.append(nn.BatchNorm1d(hidden_dim))
+            # layers.append(nn.BatchNorm1d(hidden_dim))
             layers.append(nn.LeakyReLU())
             layers.append(nn.Dropout(dropout_prob))
         
@@ -35,3 +35,34 @@ class OneHotCrossEntropyLoss(nn.Module):
         loss = -torch.sum(target_one_hot * log_probs, dim=1)
         loss = torch.mean(loss)
         return loss
+
+
+class FeatureWeightLearner(nn.Module):
+    def __init__(self, feature_dim=77, device='cpu'):
+        super(FeatureWeightLearner, self).__init__()
+        self.feature_dim = feature_dim
+        self.device = device
+        self.alpha = nn.Parameter(torch.ones(feature_dim))
+        
+    def forward(self, vec_3sec, vec_30sec):
+        vec_3sec = self.alpha * vec_3sec
+        vec_30sec = self.alpha * vec_30sec
+        return vec_3sec, vec_30sec
+        
+        
+class FeatureWeightLoss(nn.Module):
+    def __init__(self, device='cpu'):
+        super(FeatureWeightLoss, self).__init__()
+        self.device = device
+    
+    def to_numpy(self, x):
+        return x.detach().cpu().numpy()
+    
+    def to_tensor(self, x):
+        return torch.tensor(x, device=self.device)
+        
+    def forward(self, vec_3sec, vec_30sec, classify_result, label_3sec): 
+        w = torch.where(classify_result == label_3sec, 1, 0)
+        dist = torch.norm(vec_3sec - vec_30sec, p=2, dim=-2)
+        return w * dist / torch.sum(w) - (1 - w) * dist / torch.sum(1 - w)
+    
